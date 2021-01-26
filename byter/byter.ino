46f5704d3e40fdac4bcd661c462a705800733024
xinyu.zhang <zhangxinyu@igengmei.com>
2021-01-26 09:54:19 +0800

    init

---
+++ byter/byter.ino
@@ -0,0 +1,23 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Auther: mbz0x16
+// Create Date: 2021.1
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
