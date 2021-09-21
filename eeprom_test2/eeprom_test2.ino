9ec9e5ab8f067d406c139fd8f7c692bda66365ae
xinyu <35090558+Erhao@users.noreply.github.com>
2021-09-21 23:19:51 +0800

    eeprom rw finished

--- eeprom_test2/eeprom_test2.ino
+++ eeprom_test2/eeprom_test2.ino
@@ -1,5 +1,9 @@
 #include <EEPROM.h>
 
+unsigned long l;
+
+int s = sizeof(int);
+int sl = sizeof(l);
 
 void clearCount() {
   EEPROM.begin(1024);
@@ -18,15 +22,31 @@ void setup() {
   
   clearCount();
 
-  EEPROM.begin(256);
-  EEPROM.write(0, 999);
-  EEPROM.write(1, 10);
-  EEPROM.write(2, 255);
-  EEPROM.write(3, 256);
-  EEPROM.write(4, 257);
-  EEPROM.commit();
-  Serial.println("");
-  Serial.println("End write");
+  Serial.println("sizeof int:");
+  Serial.println(s);
+
+  Serial.println("sizeof unsigned long:");
+  Serial.println(sl);
+
+  EEPROM.put(1, 510);
+  int res = EEPROM.commit();
+  delay(10);
+  Serial.println("write <v1> to addr 1 ");
+  Serial.println(res);
+
+  l = 4294967294;
+  EEPROM.put(20, 2147483647);
+  int res2 = EEPROM.commit();
+  delay(10);
+  Serial.println("write <v2> to addr 20");
+  Serial.println(res2);
+
+  unsigned int ui = 2147483649;
+  EEPROM.put(30, ui);
+  int res3 = EEPROM.commit();
+  delay(10);
+  Serial.println("write <v3> to addr 30");
+  Serial.println(res3);
 
   EEPROM.begin(256);
   for (int addr = 0; addr < 256; addr ++) {
@@ -36,6 +56,25 @@ void setup() {
     delay(2);
   }
   Serial.println("End read");
+  delay(10);
+
+  int r_val1, r_val2;
+  unsigned int r_val3;
+
+  EEPROM.get(1, r_val1);
+  Serial.println("get from addr 1");
+  Serial.println(r_val1);
+
+  EEPROM.get(20, r_val2);
+  Serial.println("get from addr 20");
+  Serial.println(r_val2);
+
+  EEPROM.get(30, r_val3);
+  Serial.println("get from addr 30");
+  Serial.println(r_val3);
+
+  Serial.println("unsigned long");
+  Serial.println(l);
 }
 
 void loop() {
